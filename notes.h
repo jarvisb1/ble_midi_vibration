fe482c44fa5046044a145a4f3df6ede0b7077740
Brian Jarvis <b@bjarvis.org>
2019-12-02 23:38:44 -0500

    Update notes.h
    Fix note range

--- notes.h
+++ notes.h
@@ -7,6 +7,6 @@
 #define NUM_NOTES (16)
 
 //C Major
-const byte note_pitches[NUM_NOTES] = {pitchC6, pitchD6, pitchE6, pitchF6, pitchG6, pitchA6, pitchB6, pitchC7, pitchD7, pitchE7, pitchF7, pitchG7, pitchA7, pitchB7, pitchC8, pitchD8};
+const byte note_pitches[NUM_NOTES] = {pitchB5, pitchC6, pitchD6, pitchE6, pitchF6, pitchG6, pitchA6, pitchB6, pitchC7, pitchD7, pitchE7, pitchF7, pitchG7, pitchA7, pitchB7, pitchC8};
 
 #endif _NOTES_H_
